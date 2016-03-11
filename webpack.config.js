dd2e02a5454b3e3d5f90cbed81cace60f0ecf3ec
Eric Sauter <esauter5@gmail.com>
2016-03-11 18:27:40 -0500

    Basic project config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: "./main.js",
+
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+}
